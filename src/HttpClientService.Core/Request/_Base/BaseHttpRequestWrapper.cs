ffe19a2bddd114f775887d778c5c8def995c4ffa
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-10-28 08:45:41 +0200

    group add functions for headers and parameters

--- src/HttpClientService.Core/Request/_Base/BaseHttpRequestWrapper.cs
+++ src/HttpClientService.Core/Request/_Base/BaseHttpRequestWrapper.cs
@@ -10,20 +10,30 @@ namespace HttpClientService.Core.Request._Base
         }
 
         public string Endpoint { get; }
-        public Dictionary<string, string> RequestHeaders { get; } = new Dictionary<string, string>();
-        public Dictionary<string, string> QueryStringParameters { get; } = new Dictionary<string, string>();
+        public Dictionary<string, string> RequestHeaders { get; private set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> QueryStringParameters { get; private set;} = new Dictionary<string, string>();
 
         public TWrapper WithRequestHeader(string key, string vale)
         {
             RequestHeaders[key] = vale;
             return this as TWrapper;
         }
-        
+        public TWrapper WithRequestHeaders(Dictionary<string, string> headers)
+        {
+            RequestHeaders = headers;
+            return this as TWrapper;
+        }
+
         public TWrapper WithQueryStringParameter(string key, string vale)
         {
             QueryStringParameters[key] = vale;
             return this as TWrapper;
         }
-
+        
+        public TWrapper WithQueryStringParameters(Dictionary<string, string> parameters)
+        {
+            QueryStringParameters = parameters;
+            return this as TWrapper;
+        }
     }
 }
